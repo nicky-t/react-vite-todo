024a90851b6abcacdd0f649b5b0d2042e2731604
nicky <nickytaketo0903@gmail.com>
2022-04-03 18:18:09 +0900

    todo部分の作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,90 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useRef, useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+type Todo = {
+  id: number;
+  text: string;
+  completed: boolean;
+};
+
+const App = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  let input = useRef<HTMLInputElement>(null);
+
+  const addTodo = (text: string) => {
+    setTodos([...todos, { id: ++todos.length, text, completed: false }]);
+  };
+
+  const toggleTodo = (id: number) => {
+    console.log(id);
+    setTodos(
+      todos.map((todo) =>
+        todo.id !== id ? todo : { ...todo, completed: !todo.completed }
+      )
+    );
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+    <>
+      <Header todoCount={todos.length} />
+      <div>
+        <input ref={input} />
+        <button
+          onClick={(e) => {
+            if (!input.current?.value.trim()) return;
+            addTodo(input.current.value);
+            input.current.value = "";
+          }}
+        >
+          Add Todo
+        </button>
+      </div>
+      {todos.map((todo) => {
+        const { id, text } = todo;
+        console.log(`Creating ${text}`);
+        return (
+          <div key={id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleTodo(Number(id))}
+            />
+            <span
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+              }}
+            >
+              {text}
+            </span>
+          </div>
+        );
+      })}
+    </>
+  );
+};
 
-export default App
+const Header = ({ todoCount }: { todoCount: number }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyItems: "center",
+      }}
+    >
+      <h1>Todo List</h1>
+      <p
+        style={{
+          height: "12px",
+          lineHeight: "12px",
+          textAlign: "center",
+          marginLeft: "20px",
+        }}
+      >
+        todoCount: {todoCount}
+      </p>
+    </div>
+  );
+};
+export default App;
